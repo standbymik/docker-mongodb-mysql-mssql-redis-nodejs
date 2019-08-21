ee68bd66f2cb21654a702e5575607f42a552320f
unknown <standbypm4ever@gmail.com>
2019-08-21 10:48:48 +0700

    asyn await

--- nodejs/models/updateForumHandler.js
+++ nodejs/models/updateForumHandler.js
@@ -1,20 +1,22 @@
 import { connection } from '../connectDB/mysql'
+import { connect } from 'net';
 
 const updateHandler = async (req, res) => {
 
     const { where, title, name } = req.body
     const date = new Date()
 
-    connection.query("SELECT title FROM forum WHERE title = ?", [where], (err, results, fields) => {
+    connection.query("SELECT title FROM forum WHERE title = ?", [where], async (err, results, fields) => {
         if (err) throw err
 
         if (results.length == 0) return res.json({ success: false, error_message: 'not found' })
 
-        connection.query('UPDATE forum SET ? WHERE ?', [{ name, create_time: date }, { title: where }], (err, results, fields) => {
+        await connection.query('UPDATE forum SET ? WHERE ?', [{ name, create_time: date }, { title: where }], (err, results, fields) => { if (err) throw err })
+
+        connection.query("SELECT * FROM forum WHERE title =?", [where], (err, results) => {
             if (err) throw err
 
-            res.json({ success: true })
-
+            res.json(results)
         })
     })
 
